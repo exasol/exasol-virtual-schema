1be8cefadde07880204373774a6f6854c9eaf573
Christoph Pirkl <christoph.pirkl@exasol.com>
2021-10-04 09:08:59 +0200

    Adapt test to run with non-English locale

--- src/test/java/com/exasol/adapter/dialects/exasol/ExasolSqlGenerationVisitorTest.java
+++ src/test/java/com/exasol/adapter/dialects/exasol/ExasolSqlGenerationVisitorTest.java
@@ -5,6 +5,8 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
+import java.util.Locale;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -42,8 +44,28 @@ class ExasolSqlGenerationVisitorTest {
 
     @Test
     void testVisitSqlLiteralDouble() {
-        final ExasolSqlGenerationVisitor exasolSqlGenerationVisitor = new ExasolSqlGenerationVisitor(
-                this.exasolSqlDialect, null);
-        assertThat(exasolSqlGenerationVisitor.visit(new SqlLiteralDouble(1.23)), equalTo("CAST(1.23E0 AS DOUBLE)"));
+        final Locale defaultLocale = Locale.getDefault();
+        try {
+            // Set locale to English to avoid formatting the decimal point as comma e.g. for locale en_DE
+            Locale.setDefault(Locale.ENGLISH);
+            final ExasolSqlGenerationVisitor exasolSqlGenerationVisitor = new ExasolSqlGenerationVisitor(
+                    this.exasolSqlDialect, null);
+            assertThat(exasolSqlGenerationVisitor.visit(new SqlLiteralDouble(1.23)), equalTo("CAST(1.23E0 AS DOUBLE)"));
+        } finally {
+            Locale.setDefault(defaultLocale);
+        }
+    }
+
+    @Test
+    void testVisitSqlLiteralDoubleGermanLocale() {
+        final Locale defaultLocale = Locale.getDefault();
+        try {
+            Locale.setDefault(Locale.GERMANY);
+            final ExasolSqlGenerationVisitor exasolSqlGenerationVisitor = new ExasolSqlGenerationVisitor(
+                    this.exasolSqlDialect, null);
+            assertThat(exasolSqlGenerationVisitor.visit(new SqlLiteralDouble(1.23)), equalTo("CAST(1,23E0 AS DOUBLE)"));
+        } finally {
+            Locale.setDefault(defaultLocale);
+        }
     }
 }
