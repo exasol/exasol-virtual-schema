999daacb0232b74a9bab2221c37d46d262273e61
Christoph Pirkl <christoph.pirkl@exasol.com>
2021-12-03 15:15:34 +0100

    Add expectations for failing tests

--- src/test/java/com/exasol/adapter/dialects/exasol/ExasolSqlDialectJdbcConnectionIT.java
+++ src/test/java/com/exasol/adapter/dialects/exasol/ExasolSqlDialectJdbcConnectionIT.java
@@ -1,10 +1,18 @@
 package com.exasol.adapter.dialects.exasol;
 
+import static com.exasol.matcher.ResultSetStructureMatcher.table;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.sql.SQLException;
 import java.util.Collections;
 import java.util.Map;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import com.exasol.dbbuilder.dialects.Table;
+import com.exasol.dbbuilder.dialects.exasol.VirtualSchema;
 
 /**
  * This class exercises a set of tests defined in the base class on a local Exasol, using {@code IMPORT} via a JDBC
@@ -28,45 +36,67 @@ class ExasolSqlDialectJdbcConnectionIT extends AbstractExasolSqlDialectIT {
 
     @Override
     @Test
-    @Disabled("SPOT-13565")
     void testCastVarcharAsIntervalYearToMonth() {
-        // expected "+00004-06" but was "+04-06"
+        castFrom("VARCHAR(30)").to("INTERVAL YEAR (5) TO MONTH").input("+00004-06").accept("INTERVAL YEAR TO MONTH")
+                .verify("+04-06");
     }
 
     @Override
     @Test
-    @Disabled("SPOT-13565")
     void testCastVarcharAsIntervalDayToSecond() {
-        // expected "+00003 12:50:10.12" but was "+03 12:50:10.120"
+        castFrom("VARCHAR(30)").to("INTERVAL DAY (5) TO SECOND (2)").input("+00003 12:50:10.12")
+                .accept("INTERVAL DAY TO SECOND").verify("+03 12:50:10.120");
     }
 
     @Override
     @Test
-    @Disabled("SPOT-13565")
     void testIntervalYearToMonthMappingCustom() {
-        // expected "+005-03" but was "+05-03"
+        final Table table = createSingleColumnTable("INTERVAL YEAR (3) TO MONTH").insert("5-3");
+        assertVirtualTableContents(table, table("INTERVAL YEAR TO MONTH").row("+05-03").matches());
     }
 
     @Override
     @Test
-    @Disabled("SPOT-13565")
     void testIntervalDayToSecondMappingCustom() {
-        // expected "+0002 12:50:10.123000" but was "+02 12:50:10.123"
+        final Table table = createSingleColumnTable("INTERVAL DAY (4) TO SECOND (6)").insert("2 12:50:10.123");
+        assertVirtualTableContents(table, table("INTERVAL DAY TO SECOND").row("+02 12:50:10.123").matches());
     }
 
     @Override
     @Test
-    @Disabled("SPOT-13565")
     void testInvervalYearToMonthMappingMaxPrecision() {
-        // SQLException: ETL-3031: [Column=0 Row=0] [Interval (year to month) conversion failed for '-999999999-11' -
-        // the leading precision of the interval is too small]
+        final Table table = createSingleColumnTable("INTERVAL YEAR (9) TO MONTH")//
+                .insert("-999999999-11") //
+                .insert("-1-1") //
+                .insert("0-0") //
+                .insert("1-1") //
+                .insert("999999999-11");
+
+        final VirtualSchema virtualSchema = createVirtualSchema(this.sourceSchema);
+        try {
+            final SQLException thrown = assertThrows(SQLException.class,
+                    () -> selectAllFromCorrespondingVirtualTable(virtualSchema, table));
+            assertThat(thrown.getMessage(), containsString(
+                    "ETL-3031: [Column=0 Row=0] [Interval (year to month) conversion failed for '-999999999-11' - the leading precision of the interval is too small]"));
+        } finally {
+            virtualSchema.drop();
+        }
     }
 
     @Override
     @Test
-    @Disabled("SPOT-13565")
     void testIntervalDayToSecondMappingMaxPrecision() {
-        // SQLException: ETL-3032: [Column=0 Row=0] [Interval (day to second) conversion failed for '-999999999
-        // 23:59:59.999' - the leading precision of the interval is too small]
+        final Table table = createSingleColumnTable("INTERVAL DAY (9) TO SECOND") //
+                .insert("-999999999 23:59:59.999");
+
+        final VirtualSchema virtualSchema = createVirtualSchema(this.sourceSchema);
+        try {
+            final SQLException thrown = assertThrows(SQLException.class,
+                    () -> selectAllFromCorrespondingVirtualTable(virtualSchema, table));
+            assertThat(thrown.getMessage(), containsString(
+                    "ETL-3032: [Column=0 Row=0] [Interval (day to second) conversion failed for '-999999999 23:59:59.999' - the leading precision of the interval is too small]"));
+        } finally {
+            virtualSchema.drop();
+        }
     }
 }
