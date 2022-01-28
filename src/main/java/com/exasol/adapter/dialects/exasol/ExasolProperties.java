61b25f0060d48d5739715ad206b9d0b899b4667d
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-01-28 12:50:34 +0100

    Fix javadoc for Java 17

--- src/main/java/com/exasol/adapter/dialects/exasol/ExasolProperties.java
+++ src/main/java/com/exasol/adapter/dialects/exasol/ExasolProperties.java
@@ -4,8 +4,8 @@ package com.exasol.adapter.dialects.exasol;
  * This class contains Exasol-specific property constants.
  */
 public final class ExasolProperties {
-    public static final String EXASOL_IMPORT_PROPERTY = "IMPORT_FROM_EXA";
-    public static final String EXASOL_CONNECTION_PROPERTY = "EXA_CONNECTION";
+    static final String EXASOL_IMPORT_PROPERTY = "IMPORT_FROM_EXA";
+    static final String EXASOL_CONNECTION_PROPERTY = "EXA_CONNECTION";
 
     private ExasolProperties() {
         // prevent instantiation
