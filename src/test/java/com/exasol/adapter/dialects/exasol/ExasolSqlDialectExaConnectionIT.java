1acdf7faf3c90da153e85a26ca1bf1d50dd4b9a7
Christoph Pirkl <christoph.pirkl@exasol.com>
2024-02-08 09:30:51 +0100

    Add integration test for HASHTYPE join

--- src/test/java/com/exasol/adapter/dialects/exasol/ExasolSqlDialectExaConnectionIT.java
+++ src/test/java/com/exasol/adapter/dialects/exasol/ExasolSqlDialectExaConnectionIT.java
@@ -17,7 +17,6 @@ import org.testcontainers.containers.JdbcDatabaseContainer.NoDriverFoundExceptio
 import com.exasol.adapter.properties.PropertyValidationException;
 import com.exasol.dbbuilder.dialects.Table;
 import com.exasol.dbbuilder.dialects.exasol.ConnectionDefinition;
-import com.exasol.dbbuilder.dialects.exasol.VirtualSchema;
 
 /**
  * This class exercises a set of tests defined in the base class on a local Exasol, using {@code IMPORT} via a EXA
@@ -62,7 +61,7 @@ class ExasolSqlDialectExaConnectionIT extends AbstractRemoteExasolVirtualSchemaC
 
     @Override
     protected Set<String> expectVarcharFor() {
-        return Set.of("GEOMETRY", "INTERVAL", "INTERVAL YEAR TO MONTH", "INTERVAL DAY TO SECOND", "HASHTYPE");
+        return Set.of();
     }
 
     @Override
@@ -121,21 +120,5 @@ class ExasolSqlDialectExaConnectionIT extends AbstractRemoteExasolVirtualSchemaC
     @Test
     void testCastVarcharToChar() {
         castFrom("VARCHAR(20)").to("CHAR(40)").input("Hello.").accept("VARCHAR").verify(pad("Hello.", 40));
-    }
-
-    @Test
-    void joinHashtype() throws java.sql.SQLException {
-        final Table virtualTable = sourceSchema.createTableBuilder("VIRTUAL").column("VHASH", "HASHTYPE(16 BYTE)")
-                .build();
-        final Table realTable = objectFactory.createSchema("OTHER").createTableBuilder("REAL")
-                .column("RHASH", "HASHTYPE(16 BYTE)").build();
-        final VirtualSchema virtualSchema = createVirtualSchema(this.sourceSchema);
-        try {
-            final String sql = "select * from " + virtualSchema.getFullyQualifiedName() + "." + virtualTable.getName()
-                    + " INNER JOIN " + realTable.getFullyQualifiedName() + " ON VHASH = RHASH";
-            assertThat(query(sql), table("HASHTYPE", "HASHTYPE").matches());
-        } finally {
-            virtualSchema.drop();
-        }
     }
 }
