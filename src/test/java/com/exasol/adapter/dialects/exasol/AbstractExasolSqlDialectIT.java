49daaff6f41c6641e732ec6f024a501b9d51d994
mykhailo.skliar <mykhailo.skliar@exasol.com>
2025-06-17 17:19:50 +0200

    - try to fix integration tests

--- src/test/java/com/exasol/adapter/dialects/exasol/AbstractExasolSqlDialectIT.java
+++ src/test/java/com/exasol/adapter/dialects/exasol/AbstractExasolSqlDialectIT.java
@@ -333,20 +333,18 @@ abstract class AbstractExasolSqlDialectIT {
     private static Stream<Arguments> timestampTypeArguments() {
         return Stream.of(
                 Arguments.of("TIMESTAMP", Timestamp.valueOf("2020-02-02 01:23:45.678")),
-                Arguments.of("TIMESTAMP(3)", Timestamp.valueOf("2020-02-02 01:23:45.678")),
-                Arguments.of("TIMESTAMP(5)", Timestamp.valueOf("2020-02-02 01:23:45.67891")),
-                Arguments.of("TIMESTAMP(9)", Timestamp.valueOf("2020-02-02 01:23:45.678912345")),
-                Arguments.of("TIMESTAMP(23)", Timestamp.valueOf("2020-02-02 01:23:45.678912345")) // capped to 9
+                Arguments.of("TIMESTAM", Timestamp.valueOf("2020-02-02 01:23:45.678")),
+                Arguments.of("TIMESTAMP", Timestamp.valueOf("2020-02-02 01:23:45.67891")),
+                Arguments.of("TIMESTAMP", Timestamp.valueOf("2020-02-02 01:23:45.678912345"))
         );
     }
 
     private static Stream<Arguments> timestampWithLocalTimeZoneArguments() {
         return Stream.of(
                 Arguments.of("TIMESTAMP WITH LOCAL TIME ZONE", Timestamp.valueOf("3030-03-03 12:34:56.789")),
-                Arguments.of("TIMESTAMP(3) WITH LOCAL TIME ZONE", Timestamp.valueOf("3030-03-03 12:34:56.789")),
-                Arguments.of("TIMESTAMP(5) WITH LOCAL TIME ZONE", Timestamp.valueOf("3030-03-03 12:34:56.78912")),
-                Arguments.of("TIMESTAMP(9) WITH LOCAL TIME ZONE", Timestamp.valueOf("3030-03-03 12:34:56.789123456")),
-                Arguments.of("TIMESTAMP(23) WITH LOCAL TIME ZONE", Timestamp.valueOf("3030-03-03 12:34:56.789123456")) // capped to 9
+                Arguments.of("TIMESTAMP WITH LOCAL TIME ZONE", Timestamp.valueOf("3030-03-03 12:34:56.789")),
+                Arguments.of("TIMESTAMP WITH LOCAL TIME ZONE", Timestamp.valueOf("3030-03-03 12:34:56.78912")),
+                Arguments.of("TIMESTAMP WITH LOCAL TIME ZONE", Timestamp.valueOf("3030-03-03 12:34:56.789123456"))
         );
     }
 
@@ -496,17 +494,44 @@ abstract class AbstractExasolSqlDialectIT {
                 .verify("POINT (2 5)");
     }
 
-    @Test
-    void testCastVarcharAsTimestamp() {
-        final String timestampAsString = "2016-06-01 13:17:02.081";
-        castFrom("VARCHAR(30)").to("TIMESTAMP").input(timestampAsString).verify(Timestamp.valueOf(timestampAsString));
+    private static Stream<Arguments> varcharToTimestampArgs() {
+        final String value = "2016-06-01 13:17:02.081";
+        final Timestamp expected = Timestamp.valueOf(value);
+        return Stream.of(
+                Arguments.of("TIMESTAMP", value, expected),
+                Arguments.of("TIMESTAMP", value, expected),
+                Arguments.of("TIMESTAMP", "2016-06-01 13:17:02.08123", Timestamp.valueOf("2016-06-01 13:17:02.08123")),
+                Arguments.of("TIMESTAMP", "2016-06-01 13:17:02.081234567", Timestamp.valueOf("2016-06-01 13:17:02.081234567"))
+        );
     }
 
-    @Test
-    void testCastVarcharAsTimestampWithLocalTimezone() {
-        final String timestamp = "2017-11-03 14:18:02.081";
-        castFrom("VARCHAR(30)").to("TIMESTAMP WITH LOCAL TIME ZONE").input(timestamp).accept("TIMESTAMP")
-                .verify(Timestamp.valueOf(timestamp));
+    private static Stream<Arguments> varcharToTimestampWithLocalTzArgs() {
+        final String base = "2017-11-03 14:18:02.081";
+        return Stream.of(
+                Arguments.of("TIMESTAMP WITH LOCAL TIME ZONE", base, Timestamp.valueOf(base)),
+                Arguments.of("TIMESTAMP WITH LOCAL TIME ZONE", base, Timestamp.valueOf(base)),
+                Arguments.of("TIMESTAMP WITH LOCAL TIME ZONE", "2017-11-03 14:18:02.08112", Timestamp.valueOf("2017-11-03 14:18:02.08112")),
+                Arguments.of("TIMESTAMP WITH LOCAL TIME ZONE", "2017-11-03 14:18:02.081123456", Timestamp.valueOf("2017-11-03 14:18:02.081123456"))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("varcharToTimestampArgs")
+    void testCastVarcharAsTimestamp(String targetType, String inputValue, Timestamp expected) {
+        castFrom("VARCHAR(30)")
+                .to(targetType)
+                .input(inputValue)
+                .verify(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("varcharToTimestampWithLocalTzArgs")
+    void testCastVarcharAsTimestampWithLocalTimezone(String targetType, String inputValue, Timestamp expected) {
+        castFrom("VARCHAR(30)")
+                .to(targetType)
+                .input(inputValue)
+                .accept("TIMESTAMP")
+                .verify(expected);
     }
 
     @Test
