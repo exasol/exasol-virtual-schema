a18e3104d28ac2e669924db57c8e64e2ac6cf8c7
Christoph Pirkl <christoph.pirkl@exasol.com>
2026-06-08 07:54:17 +0200

    Use execute instead of query

--- src/test/java/com/exasol/adapter/dialects/exasol/ExasolSqlDialectExaConnectionWithDataTypesIT.java
+++ src/test/java/com/exasol/adapter/dialects/exasol/ExasolSqlDialectExaConnectionWithDataTypesIT.java
@@ -10,9 +10,7 @@ import java.sql.SQLException;
 import java.util.Map;
 import java.util.Set;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.testcontainers.containers.JdbcDatabaseContainer.NoDriverFoundException;
 
 import com.exasol.adapter.properties.PropertyValidationException;
@@ -89,7 +87,7 @@ class ExasolSqlDialectExaConnectionWithDataTypesIT extends AbstractRemoteExasolV
         this.testVirtualSchema = createVirtualSchema(this.sourceSchema);
         final String name = this.testVirtualSchema.getFullyQualifiedName();
         final SQLException exception = assertThrows(SQLException.class,
-                () -> query("alter virtual schema {0} set EXA_CONNECTION = Null", name));
+                () -> execute("alter virtual schema {0} set EXA_CONNECTION = Null", name));
         final String expected = PropertyValidationException.class.getName() + ": E-VSCJDBC-17";
         assertThat(exception.getMessage(), containsString(expected));
     }
